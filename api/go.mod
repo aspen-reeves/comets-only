8955fc42f3a690e8b292ddb59c3d023436a68ad3
aspen <aspenbreeves@gmail.com>
2022-11-13 10:14:11 -0600

    waaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- api/go.mod
+++ api/go.mod
@@ -1,7 +1,5 @@
-module api
+module main
 
 go 1.19
 
 require github.com/gorilla/mux v1.8.0
-
-require github.com/rs/cors v1.8.2
--- api/main.go
+++ api/main.go
@@ -1,9 +1,7 @@
 package main
 
-import "api/api"
-
 func main() {
 
 	// random data for profile array
-	api.HandleRequests()
+	HandleRequests()
 }
