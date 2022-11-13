0c1266b8f391ce107a31e2db7328930654c9b8d1
aspen <aspenbreeves@gmail.com>
2022-11-12 22:15:35 -0600

    kgkgnynyugynuguonyigngungnugnuiu

--- api/data.go
+++ api/data.go
@@ -1,17 +1,9 @@
-package main
+package api
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"math/rand"
-	"net/http"
 	"os"
-	"strconv"
-	"time"
-
-	"github.com/gorilla/mux"
 )
 
 // main struct for holding profile data
@@ -118,74 +110,4 @@ func cringeToJSON(temp profile) profileJSON {
 	tempJSON.LastShower = temp.LastShower
 	tempJSON.Code = temp.Code
 	return tempJSON
-}
-
-// landing page
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-// returns a profile with a given id
-func returnProfile(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-	idTemp, _ := strconv.Atoi(key)
-	if idTemp > len(profiles) {
-		fmt.Fprintf(w, "Profile not found")
-		return
-	}
-	tempJSON := cringeToJSON(profiles[idTemp])
-	fmt.Println("Endpoint Hit: returnProfile")
-	fmt.Println(tempJSON)
-	fmt.Println(profiles[idTemp])
-	json.NewEncoder(w).Encode(tempJSON)
-}
-
-// APT format
-// {"name":"","age":,"lang":"","os":"","editor":""}
-// {"name":"joe mama","age":69,"lang":"js","os":"Linux","editor":"Vim", "lastShower":"2020-10-10T10:10:10Z", "code":"console.log(\"Hello World\")"}
-
-// create a profile from a POST request
-func createProfile(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var temp profile
-	json.Unmarshal(reqBody, &temp)
-	profiles = append(profiles, temp)
-	fmt.Println(temp)
-	saveProfile(profiles)
-	fmt.Println("Endpoint Hit: createProfile")
-	json.NewEncoder(w).Encode(temp)
-}
-
-// create api call for random person
-func getBitches(w http.ResponseWriter, r *http.Request) {
-	// return a random person
-	fmt.Println("Endpoint Hit: getBitches")
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-
-	json.NewEncoder(w).Encode(profiles[r1.Intn(len(profiles))])
-
-}
-
-// function for handling requests
-func handleRequests() {
-	myRouter := mux.NewRouter().StrictSlash(true)
-	//default route
-	myRouter.HandleFunc("/", homePage)
-	//route to get a profile
-	myRouter.HandleFunc("/profile/{id}", returnProfile)
-	//route to create a profile
-	myRouter.HandleFunc("/signup", createProfile).Methods("POST")
-	//route to get a random profile
-	myRouter.HandleFunc("/getBitches", getBitches)
-
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
-}
-
-func main() {
-
-	// random data for profile array
-	handleRequests()
 }
